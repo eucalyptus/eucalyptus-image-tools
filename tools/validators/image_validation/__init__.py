4c36340f0a8b13e06fd56938736c920d828b950b
Jeff Uphoff <juphoff@eucalyptus.com>
2013-07-31 13:12:47 -0400

    drive functionality from command-line arguments

--- tools/validators/image_validation/__init__.py
+++ tools/validators/image_validation/__init__.py
@@ -3,16 +3,20 @@ import getopt
 import guestfs
 import threading
 
+def _usage():
+    print '\nUsage:'
+    print '%s [-a image] [--check-dependencies] mountpoint\n' % sys.argv[0]
+    sys.exit(0)                         # By spec, exit 0 unless bad validation.
+    
 def _mount_local_run(guest):
     print "calling guestfs.mount_local_run()"
     guest.mount_local_run()
     print "guestfs.mount_local_run() returned"
     
-def _mountFUSE(image, trace=False):
+def _mountFUSE(image, mountpoint, trace=False):
     guest = guestfs.GuestFS()
 
     if trace:
-        pass
         guest.set_trace(1)
 
     guest.add_drive_opts(image, readonly=1)
@@ -31,7 +35,8 @@ def _mountFUSE(image, trace=False):
             except RuntimeError as msg:
                 print "%s (ignored)" % msg
 
-    guest.mount_local ('mnt')
+    # FIXME: Create this mount point if it doesn't already exist.
+    guest.mount_local (mountpoint)
 
     # FIXME: Add conditional to ensure root is mounted?
     runThread = threading.Thread(target=_mount_local_run, args=(guest,))
@@ -46,28 +51,34 @@ def _mountRaw(image):
     pass
 
 class ImageAccess():
-    def __init__(self, image=None, libguestfs=False, fuse=False, trace=False):
+    def __init__(self, trace=False):
+        self.image = None
+        self._trace = trace
+        
         try:
-            optlist, args = getopt.getopt(sys.argv[1:], None,
-                                          ['check-dependencies'])
+            optlist, arglist = getopt.getopt(sys.argv[1:], 'a:',
+                                          ['check-dependencies', 'trace'])
         except getopt.GetoptError as e:
-            print '\n%s: %s\n' % (sys.argv[0], e)
-            exit                        # By spec, exit 0 unless bad validation.
-
-        #print 'optlist: ', optlist
-        #print 'args: ', args
+            print '\n%s: %s' % (sys.argv[0], e)
+            _usage()
 
         for o, a in optlist:
             if o == '--check-dependencies':
                 self.check_dependencies = True
+            elif o == '-a':
+                self.image = a
+            elif o == '--trace':
+                self._trace = True
+            else:
+                _usage()
 
-        if not image:
-            self.image = args[0]
+        if self.image:
+            self.mountpoint = arglist[0]
+            import epdb ; epdb.set_trace()
+            self.guest = _mountFUSE(self.image, self.mountpoint,
+                                    trace=self._trace)
         else:
-            self.image = image
-
-        if fuse:
-            libguestfs = True
-            self.guest = _mountFUSE(self.image, trace=trace)
+            print '\n%s: Usage without images not yet supported.\n' % sys.argv[0]
+            sys.exit(0)
 
         import epdb ; epdb.set_trace()
