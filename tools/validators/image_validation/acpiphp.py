3be5ccabd3360279e5d7f75746f8c94265383ad4
Jeff Uphoff <juphoff@eucalyptus.com>
2013-08-01 17:05:52 -0400

    Perform search for module.

--- tools/validators/image_validation/acpiphp.py
+++ tools/validators/image_validation/acpiphp.py
@@ -1,4 +1,16 @@
+import os
+import sys
 from image_validation import ImageAccess
 
 def validator(trace=False):
     val = ImageAccess(trace)
+    #import epdb ; epdb.st()
+    modules = os.walk('%s/lib/modules/' % val.mountpoint)
+    acpiphp = [x for x in modules if 'acpiphp.ko' in x[2]]
+
+    del val
+
+    if len(acpiphp):
+        sys.exit(0)
+    else:
+        sys.exit(1)
