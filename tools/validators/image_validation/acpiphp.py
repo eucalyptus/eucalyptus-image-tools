7f2f4c1cc203caf1e24047e444af6bf4f47c4d33
Jeff Uphoff <juphoff@eucalyptus.com>
2013-08-02 14:13:59 -0400

    Add verbose output and proper clean-up.

--- tools/validators/image_validation/acpiphp.py
+++ tools/validators/image_validation/acpiphp.py
@@ -2,15 +2,21 @@ import os
 import sys
 from image_validation import ImageAccess
 
+moduleName = 'acpiphp.ko'
+
 def validator(trace=False):
     val = ImageAccess(trace)
     #import epdb ; epdb.st()
-    modules = os.walk('%s/lib/modules/' % val.mountpoint)
-    acpiphp = [x for x in modules if 'acpiphp.ko' in x[2]]
+    if val.mounted:
+        modules = os.walk('%s/lib/modules/' % val.mountpoint)
+        acpiphp = [x for x in modules if moduleName in x[2]]
 
-    del val
-
-    if len(acpiphp):
-        sys.exit(0)
-    else:
-        sys.exit(1)
+        if len(acpiphp):
+            #import epdb ; epdb.st()
+            val.qprint('Found: %s' % ['%s/%s' % (x[0], x[2][0]) for x in acpiphp][0])
+            del val                         # Important for FUSE cleanup.
+            sys.exit(0)
+        else:
+            val.qprint('Did not find: %s' % moduleName)
+            del val
+            sys.exit(1)
