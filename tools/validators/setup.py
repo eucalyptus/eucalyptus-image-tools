30af96e52233dbfd98f5c7799aaa6c5d41382697
Jeff Uphoff <juphoff@eucalyptus.com>
2013-08-19 16:10:22 -0400

    Add setup.py & .gitignore.

---
+++ tools/validators/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='image_validation',
+      version='0.1',
+      packages=['euca_image_validation',],
+      scripts=['euca_image_validate.py',],
+      url='https://github.com/eucalyptus/eucalyptus-image-tools',
+      author='Jeff Uphoff',
+      author_email='juphoff@eucalyptus.com',
+      description='Image-validation tools for Eucalyptus',
+      license='BSD',
+      )
