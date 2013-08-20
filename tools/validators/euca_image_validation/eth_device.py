5ca4fa290ba1e0b2a24af9a3638b3d18f93ce757
Jeff Uphoff <juphoff@eucalyptus.com>
2013-08-20 16:21:58 -0400

    Implement unmounted Ubuntu validator, stub Red Hat validators.

--- tools/validators/euca_image_validation/eth_device.py
+++ tools/validators/euca_image_validation/eth_device.py
@@ -1,26 +1,32 @@
 import os
 import sys
 
-# fileName = 'interfaces'
-# fileBase = '/etc/network'
+fileName = 'interfaces'
+dirName = '/etc/network'
 
-def _check_redhat(val):
+def _check_redhat_mounted(val):
     # FIXME: implement
+    val.vprint('Note: eth_device check not yet implemented for mounted Red Hat images.')
     return False
 
-def _check_ubuntu(val):
-    fileName = '%s/etc/network/interfaces' % val.mountpoint
+def _check_redhat_unmounted(val):
+    # FIXME: implement
+    val.vprint('Note: eth_device check not yet implemented for unmounted Red Hat images.')
+    return False
+
+def _check_ubuntu_mounted(val):
+    fullPath = '%s/%s/%s' % (val.get_mountpoint(), dirName, fileName)
 
     try:
-        f = open(fileName, 'r')
+        f = open(fullPath, 'r')
     except Exception as e:
-        val.qprint("Cannot open file '%s': %s" % (fileName, e))
+        val.qprint("Cannot open file '%s': %s" % (fullPath, e))
         return False
     
     fileContents = []
     fileContents = f.readlines()
 
-    interfaces = [x for x in fileContents if x.startswith('auto eth')]
+    interfaces = [x.strip() for x in fileContents if x.strip().startswith('auto eth')]
 
     for interface in interfaces:
         val.qprint('Found interface: %s' % interface)
@@ -31,15 +37,38 @@ def _check_ubuntu(val):
         val.qprint('Did not find any automatically enabled Ethernet interfaces.')
         return False
 
+def _check_ubuntu_unmounted(val):
+    fullPath = '%s/%s' % (dirName, fileName)
+
+    try:
+        fileContents = val.guest.read_lines(fullPath)
+    except Exception as e:
+        val.qprint("Cannot read file '%s': %s" % (fullPath, e))
+        return False
+
+    interfaces = [x.strip() for x in fileContents if x.strip().startswith('auto eth')]
+
+    for interface in interfaces:
+        val.qprint('Found interface: %s' % interface)
+
+    if len(interfaces):
+        return True
+    else:
+        val.qpring('Did not find any automatically enabled Ethernet interfaces.')
+        return False
+
 def validator(val, trace=False):
-    if val.mounted:
-        if _check_redhat(val):
+    if val.is_mounted():
+        if _check_redhat_mounted(val):
             return True
-        elif _check_ubuntu(val):
+        elif _check_ubuntu_mounted(val):
             return True
         else:
             return False
     else:
-        val.qprint('Note: eth_device check not yet implemented for umounted images.')
-        # FIXME: implement
-        return True
+        if _check_redhat_unmounted(val):
+            return True
+        elif _check_ubuntu_unmounted(val):
+            return True
+        else:
+            return False
