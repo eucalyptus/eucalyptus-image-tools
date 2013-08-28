0e77045694d7264b36a23efcbbcd46ba29686237
Jeff Uphoff <juphoff@eucalyptus.com>
2013-08-27 21:17:03 -0400

    Move validation scripts to modules.

--- tools/validators/validate_acpihp.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-from image_validation import acpiphp
-
-acpiphp.validator(trace=False)
--- tools/validators/validate_eth_device.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-from image_validation import eth_device
-
-eth_device.validator(trace=False)
