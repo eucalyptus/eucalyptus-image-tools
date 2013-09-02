dbdaa1689e40321c91f8f8e827311c7c45a45f39
Jeff Uphoff <juphoff@eucalyptus.com>
2013-09-02 18:59:27 -0400

    PEP 8.

--- tools/validators/euca_image_validation/eth_device.py
+++ tools/validators/euca_image_validation/eth_device.py
@@ -2,53 +2,55 @@ import glob
 import os
 import sys
 
-# FIXME: clean this up.
-redhatDirName = '/etc/sysconfig/network-scripts'
-redhatFileStub = 'ifcfg-eth'
-redhatFilePrefix = '%s/%s' % (redhatDirName, redhatFileStub)
+redhat_dirname = '/etc/sysconfig/network-scripts'
+redhat_file_prefix = 'ifcfg-eth'
 
-ubuntuFileName = 'interfaces'
-ubuntuDirName = '/etc/network'
+ubuntu_dirname = '/etc/network'
+ubuntu_filename = 'interfaces'
 
 def _check_redhat(val):
-    retVal = False
-    filesContents = {}
-    ifFiles = val.find_files(redhatDirName, '%s*' % redhatFileStub, glob=True, omit_mountpoint=False)
+    retval = False
+    files_contents = {}
 
-    for ifFile in ifFiles:
+    if_files = val.find_files(redhat_dirname, '%s*' % redhat_file_prefix, glob=True, omit_mountpoint=False)
+
+    for if_file in if_files:
         try:
-            fileContents = val.read_file(ifFile)
-            if not len(fileContents):
+            file_contents = val.read_file(if_file)
+            if not len(file_contents):
                 continue
-            filesContents[ifFile] = {}
+            files_contents[if_file] = {}
 
-            for line in fileContents:
-                lineTup = tuple(line.strip().split('='))
-                filesContents[ifFile][lineTup[0]] = lineTup[1]
+            for line in file_contents:
+                line_tup = tuple(line.strip().split('='))
+                files_contents[if_file][line_tup[0]] = line_tup[1]
 
-            val.vprint('Checking interface: %s' % filesContents[ifFile]['DEVICE'])
-            if filesContents[ifFile]['ONBOOT'].count('yes') or filesContents[ifFile]['ONBOOT'].count('on'):
-                val.qprint('Found interface: %s' % filesContents[ifFile]['DEVICE'])
-                retVal = True
+            # FIXME: handle pathological case of interface file with no DEVICE
+            # and/or ONBOOT line(s).
+            val.vprint('Checking interface: %s' % files_contents[if_file]['DEVICE'])
+            if files_contents[if_file]['ONBOOT'].count('yes') or files_contents[if_file]['ONBOOT'].count('on'):
+                val.qprint('Found interface: %s' % files_contents[if_file]['DEVICE'])
+                retval = True
         except Exception as e:
-            val.qprint("Cannot analyze file '%s': %s" % (ifFile, e))
+            val.qprint("Cannot analyze file '%s': %s" % (if_file, e))
 
-    return retVal
+    return retval
 
 def _check_ubuntu(val):
-    pathList = val.find_files(ubuntuDirName, ubuntuFileName, omit_mountpoint=False)
-    if len(pathList):
-        fullPath = pathList[0]
+    path_list = val.find_files(ubuntu_dirname, ubuntu_filename, omit_mountpoint=False)
+
+    if len(path_list):
+        full_path = path_list[0]
     else:
         return False
 
     try:
-        fileContents = val.read_file(fullPath)
+        file_contents = val.read_file(full_path)
     except Exception as e:
-        val.qprint("Cannot analyze file '%s': %s" % (fullPath, e))
+        val.qprint("Cannot analyze file '%s': %s" % (full_path, e))
         return False
 
-    interfaces = [x.strip() for x in fileContents if x.strip().startswith('auto eth')]
+    interfaces = [x.strip() for x in file_contents if x.strip().startswith('auto eth')]
 
     for interface in interfaces:
         val.qprint('Found interface: %s' % interface)
